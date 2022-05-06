e1d0a3f1cff5c018861797b9585c8ce05385b2d4
NeodymiumOre <maciejbarna@gmail.com>
2022-05-06 11:43:16 +0200

    Working LCD test code

--- wmt_code/src/main.cpp
+++ wmt_code/src/main.cpp
@@ -1,10 +1,26 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+// Create an LCD object. Parameters: (RS, E, D4, D5, D6, D7):
+LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Specify the LCD's number of columns and rows. Change to (20, 4) for a 20x4 LCD:
+  lcd.begin(16, 2);
 }
 
 void loop() {
+  // Set the cursor on the third column and the first row, counting starts at 0:
+  lcd.setCursor(2, 0);
+  // Print the string 'Hello World!':
+  lcd.print("Hello World!");
+  // Set the cursor on the third column and the second row:
+  lcd.setCursor(2, 1);
+  // Print the string 'LCD tutorial':
+  lcd.print("LCD tutorial");
+  
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
